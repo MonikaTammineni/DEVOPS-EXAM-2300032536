74cd76531316becfe40b3b72443219998bcc3e12
MonikaTammineni <2300032536@kluniversity.in>
2025-10-18 10:18:14 +0530

    changes made

--- frontend-reactapp/vite.config.js
+++ frontend-reactapp/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/2300032536-frontend/',
+  base: 'DEVOPS-EXAM-2300032536',
 })
